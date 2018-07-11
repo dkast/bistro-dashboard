6d92388802c33dd23e256bb3cc85c89a1257aefb
Daniel Castillejo <devcastillejo@gmail.com>
2018-07-11 00:51:55 -0500

    Configure redux-firestore

--- store/index.js
+++ store/index.js
@@ -1,8 +1,13 @@
-import { createStore } from "redux";
+import { createStore, combineReducers, compose } from "redux";
+import { reduxFirestore } from "redux-firestore";
 import rootReducer from "./reducers";
+import { firebase } from "../firebase";
+
+// Add reduxFirestore store enhancer to store createtor
+const createStoreWithFirebase = compose(reduxFirestore(firebase))(createStore);
 
 const initStore = () => {
-  const store = createStore(rootReducer);
+  const store = createStoreWithFirebase(rootReducer);
 
   return store;
 };
--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from "redux";
+import { firestoreReducer } from "redux-firestore";
 import sessionReducer from "./session";
 import userReducer from "./user";
 
 const rootReducer = combineReducers({
   sessionState: sessionReducer,
-  userState: userReducer
+  userState: userReducer,
+  firestore: firestoreReducer
 });
 
 export default rootReducer;
